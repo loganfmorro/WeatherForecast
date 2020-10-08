4c99015b2c763ee3151ed901bfc71d48c5a68658
Logan Morro <loganfmorro@gmail.com>
2020-10-07 22:40:27 -0400

    I can't get anything to work and I am beyond frustrated at this point

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 $("#searchBtn").click(function () {
+    
 
     //This is our input variable
     var userInput = $("#userInputCity").val();
@@ -14,7 +15,6 @@ $("#searchBtn").click(function () {
     var currentDate = twoDigitMonth + "/" + twoDigitDate + "/" + fullDate.getFullYear();console.log(currentDate);
 
     //Forecast Icon /URL
-    //var forecastIcon = 
     
     // This is our API key
     var APIKey = "d74cc4a76e70093d75fd423bfd5183ce";
@@ -38,18 +38,42 @@ $("#searchBtn").click(function () {
             console.log(response);
 
             // Transfer content to HTML
-            $("#cityRow").html("<h4>" + response.name + " " + currentDate + "</h4>" + " <img src= http://openweathermap.org/img/wn/" + response.weather.icon + "@2x.png>");
+                //City/Date/Symbol
+            $("#cityRow").html("<h4>" + response.name + " " + currentDate + "</h4>" + " <img src= http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png>");
+                //Wind Speed
             $("#windSpeedRow").text("Wind Speed: " + response.wind.speed);
+                //Humidity
             $("#humidityRow").text("Humidity: " + response.main.humidity);
+
                 // Convert the temp to fahrenheit
             var tempF = (response.main.temp - 273.15) * 1.80 + 32;
                 // add temp content to html
             $("#tempRow").text("Temperature: " + tempF.toFixed(2) + " (F)");
+
+                // 5 day dates
+            $("#dateHeader1").text(response.list.dt[1]);
+            $("#dateHeader2").text(response.list.dt[2]);
+            $("#dateHeader3").text(response.list.dt[3]);
+            $("#dateHeader4").text(response.list.dt[4]);
+            $("#dateHeader5").text(response.list.dt[5]);
 
             // Log the data in the console as well
             console.log("Wind Speed: " + response.wind.speed);
             console.log("Humidity: " + response.main.humidity);
             console.log("Temperature (F): " + tempF);
             console.log("Icon: " + response.weather.icon);
+            console.log("Date: ") + response.list.dt;
+
         });
+        //saving recent search
+        localStorage.setItem("lastCity", userInput);
+
+        var localStorageCities = JSON.parse(localStorage.getItem("lastCity"));
+
+        for (let i = 0; i < localStorageCities.length; i++) {
+            var ptag = document.createElement("p"); // Create a <p> node
+            var textnode = document.createTextNode(localStorageTimes[i]); // Create a text node
+            ptag.appendChild(textnode); // Append the text to p tag
+            document.getElementById("recentCities").appendChild(ptag);
+        }
 });
